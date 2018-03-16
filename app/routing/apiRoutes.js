79b67353293499cd7f7dcfae23be41c5120b43d0
Katherine Rinas <katierinas@me.com>
2018-03-16 14:51:14 -0600

    Fixing closing }

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,50 +3,52 @@ var friends = require("../data/friends.js");
 
 module.exports= function (app) {
 
-app.get("/api/friends", function(req, res) {
-	res.json(friends);
-});
+	app.get("/api/friends", function(req, res) {
+		res.json(friends);
+	});
 
-app.post("/api/friends", function(req, res) {
+	app.post("/api/friends", function(req, res) {
 
-	var bestFriend= {
-		name: "",
-		photo: "",
-	    friendDifference:1000
-};
-
-
-	console.log (req.body);
-
-	var userData = req.body;
-	var userScores = userData.scores;
-
-	console.log (userScores);
-
-var totalDifference = 0;
-
-for (var i = 0; i < friends.length; i++) {
-	console.log (friends [i]);
-	totalDifference = 0;
-
-	for (var j = 0; i < friends[i].scores[j]; j++){
+		var bestFriend= {
+			name: "",
+			photo: "",
+			friendDifference:1000
+		};
 
 
+		console.log (req.body);
 
-		totalDifference += math.abs(parseInt(userScores[j])-parseInt(friends[i].scores[j]));
+		var userData = req.body;
+		var userScores = userData.scores;
 
-		if (totalDifference <= bestFriend.friendDifference) {
+		console.log (userScores);
 
-			bestMatch.name = friends[i].name;
-			bestMatch.photo = friends[i].photo;
-			bestMatch.friendDifference = totalDifference;
+		var totalDifference = 0;
+
+		for (var i = 0; i < friends.length; i++) {
+			console.log (friends [i]);
+			totalDifference = 0;
+
+			for (var j = 0; i < friends[i].scores[j]; j++){
 
 
 
+				totalDifference += math.abs(parseInt(userScores[j])-parseInt(friends[i].scores[j]));
+
+				if (totalDifference <= bestFriend.friendDifference) {
+
+					bestMatch.name = friends[i].name;
+					bestMatch.photo = friends[i].photo;
+					bestMatch.friendDifference = totalDifference;
+
+
+
+				}
+			}
 		}
-	}
-  }
-});
 
-	friends.push(userData);
- 
+		res.send(bestMatch);
+		friends.push(userData);
+	});
+
+};
